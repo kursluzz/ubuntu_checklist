04454aa203da6111cb921235418b3aef72b783fd
oleg <oleg@bondit.co.il>
2020-02-04 12:06:54 +0200

    shutter fix

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -333,7 +333,7 @@ if [ "$INSTALL_SHUTTER" -eq 1 ]; then
   wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas-common_1.0.0-1_all.deb
   dpkg -i libgoocanvas-common_1.0.0-1_all.deb
   rm libgoocanvas-common_1.0.0-1_all.deb
-  https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
+  wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoocanvas3_1.0.0-1_amd64.deb
   dpkg -i libgoocanvas3_1.0.0-1_amd64.deb
   rm libgoocanvas3_1.0.0-1_amd64.deb
   wget https://launchpad.net/ubuntu/+archive/primary/+files/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
