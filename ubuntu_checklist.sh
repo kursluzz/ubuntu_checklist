5625b01f507a93a2c52d26272def476185903dab
Oleg <oleg@work>
2020-07-22 10:48:51 +0300

    minor bash fixes and --restart=unless-stopped

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -93,30 +93,12 @@ INSTALL_MYSQL_DOCKER=1
 INSTALL_ACTIVEMQ_DOCKER=1
 INSTALL_MONGODB_DOCKER=1
 
-# dependencies
-# todo: implement smart dependency will check if dependency exists before installing it
-#if [ $INSTALL_DOCKER -eq 1 ]; then
-#    INSTALL_CURL=1
-#fi
-
-#if [ $INSTALL_VENV -eq 1 ]; then
-#    INSTALL_PIP3=1
-#fi
-
-#if [ $INSTALL_SERVERLESS -eq 1 ]; then
-#    INSTALL_NODEJS_NPM=1
-#fi
-
-#if [ $INSTALL_ANGULAR_CLI -eq 1 ]; then
-#    INSTALL_NODEJS_NPM=1
-#fi
-
 # FUNCTIONS
 get_os_version_id() {
   cat /etc/os-release | while read line; do
-    if [[ $line == "VERSION_ID="* ]]; then
+    if [[ ${line} == "VERSION_ID="* ]]; then
       eval "local $line"
-      echo $VERSION_ID
+      echo ${VERSION_ID}
       break
     fi
   done
@@ -124,57 +106,57 @@ get_os_version_id() {
 
 # MAIN RUN
 # must run as sudo check
-if [ "$EUID" -ne 0 ]; then
+if [[ "$EUID" -ne 0 ]]; then
   echo Please run as sudo
   exit
 fi
 
-sudo -u $MYUSER mkdir -p /home/$MYUSER/Downloads
-cd /home/$MYUSER/Downloads
+sudo -u ${MYUSER} mkdir -p /home/${MYUSER}/Downloads
+cd /home/${MYUSER}/Downloads
 
 apt -y update
 apt -y upgrade
 
-if [ "$INSTALL_CURL" -eq 1 ]; then
+if [[ "$INSTALL_CURL" -eq 1 ]]; then
   echo ---------- Installing curl
   apt -y install curl
 fi
 
-if [ "$INSTALL_VIM" -eq 1 ]; then
+if [[ "$INSTALL_VIM" -eq 1 ]]; then
   echo ---------- Installing vim
   apt -y install vim
 fi
 
-if [ "$INSTALL_BYOBU" -eq 1 ]; then
+if [[ "$INSTALL_BYOBU" -eq 1 ]]; then
   echo ---------- Installing byobu
   apt -y install byobu
 fi
 
-if [ "$INSTALL_WAKEONLAN" -eq 1 ]; then
+if [[ "$INSTALL_WAKEONLAN" -eq 1 ]]; then
   echo ---------- Installing wakeonlan
   apt -y install wakeonlan
 fi
 
-if [ "$INSTALL_MAKE" -eq 1 ]; then
+if [[ "$INSTALL_MAKE" -eq 1 ]]; then
   echo ---------- Installing make
   apt -y install make
 fi
 
-if [ "$INSTALL_SSH_SERVER" -eq 1 ]; then
+if [[ "$INSTALL_SSH_SERVER" -eq 1 ]]; then
   echo ---------- Installing SSH server
   apt -y install openssh-server
 fi
 
-if [ "$INSTALL_GIT" -eq 1 ]; then
+if [[ "$INSTALL_GIT" -eq 1 ]]; then
   echo ---------- Installing git
   apt -y install git
 
   echo ---------- Setting git global user and password
-  sudo -u $MYUSER git config --global user.email "$MYGITEMAIL"
-  sudo -u $MYUSER git config --global user.name "$MYGITNAME"
+  sudo -u ${MYUSER} git config --global user.email "$MYGITEMAIL"
+  sudo -u ${MYUSER} git config --global user.name "$MYGITNAME"
 fi
 
-if [ "$INSTALL_PYTHON36" -eq 1 ]; then
+if [[ "$INSTALL_PYTHON36" -eq 1 ]]; then
   # https://towardsdatascience.com/building-python-from-source-on-ubuntu-20-04-2ed29eec152b
   echo ---------- Installing python 3.6
   apt update
@@ -198,39 +180,39 @@ if [ "$INSTALL_PYTHON36" -eq 1 ]; then
   #  rm Python-3.6.9.tgz
 fi
 
-if [ "$INSTALL_MYSQL_PYTHON_DEPENDENCIES" -eq 1 ]; then
+if [[ "$INSTALL_MYSQL_PYTHON_DEPENDENCIES" -eq 1 ]]; then
   echo ---------- Installing mysql dev dependencies
   apt -y install libmysqlclient-dev
 fi
 
-if [ "$INSTALL_PIP3" -eq 1 ]; then
+if [[ "$INSTALL_PIP3" -eq 1 ]]; then
   echo ---------- Installing pip3
   apt -y install python3-pip
 fi
 
-if [ "$INSTALL_NGINX" -eq 1 ]; then
+if [[ "$INSTALL_NGINX" -eq 1 ]]; then
   echo ---------- Installing nginx
   apt -y install nginx
 
 fi
 
-if [ "$INSTALL_VENV" -eq 1 ]; then
+if [[ "$INSTALL_VENV" -eq 1 ]]; then
   echo ---------- Installing VENV
   apt -y install python3-venv
 fi
 
-if [ "$INSTALL_AWS_CLI" -eq 1 ]; then
+if [[ "$INSTALL_AWS_CLI" -eq 1 ]]; then
   echo ---------- Installing AWS CLI
   # pip3 install awscli --upgrade --user
   snap install aws-cli --classic
 fi
 
-if [ "$INSTALL_AWS_EB" -eq 1 ]; then
+if [[ "$INSTALL_AWS_EB" -eq 1 ]]; then
   echo ---------- Installing AWS EB
   pip3 install --upgrade awsebcli
 fi
 
-if [ "$INSTALL_NODEJS_NPM" -eq 1 ]; then
+if [[ "$INSTALL_NODEJS_NPM" -eq 1 ]]; then
   echo ---------- Installing nodejs npm
   # https://linuxize.com/post/how-to-install-node-js-on-ubuntu-18.04/
   # curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
@@ -238,71 +220,71 @@ if [ "$INSTALL_NODEJS_NPM" -eq 1 ]; then
   apt install -y nodejs
 fi
 
-if [ "$INSTALL_ANGULAR_CLI" -eq 1 ]; then
+if [[ "$INSTALL_ANGULAR_CLI" -eq 1 ]]; then
   echo ---------- Installing Angular CLI
   #npm install -g @angular/cli
   printf "\n" | npm install -g @angular/cli
 fi
 
-if [ "$INSTALL_SERVERLESS" -eq 1 ]; then
+if [[ "$INSTALL_SERVERLESS" -eq 1 ]]; then
   echo ---------- Installing serverless
   npm install -g serverless
 fi
 
-if [ "$INSTALL_JDK" -eq 1 ]; then
+if [[ "$INSTALL_JDK" -eq 1 ]]; then
   echo ---------- Installing JDK11
   sudo apt -y install openjdk-11-jdk-headless
 fi
 
-if [ "$ADD_SSH_KEY_FOR_GIT" -eq 1 ]; then
+if [[ "$ADD_SSH_KEY_FOR_GIT" -eq 1 ]]; then
   echo ---------- Adding SSH key for git
   DO_SSH=0
-  if [ -e /home/$MYUSER/.ssh/id_rsa ]; then
+  if [[ -e /home/${MYUSER}/.ssh/id_rsa ]]; then
     read -p 'File ~/.ssh/id_rsa already exists! Do you want do delete it? [Y/n]: ' CH
-    if [ "$CH" = '' ] || [ "$CH" = 'y' ] || [ "$CH" = 'Y' ]; then
-      sudo -u $MYUSER rm ~/.ssh/id_rsa*
+    if [[ "$CH" = '' ]] || [[ "$CH" = 'y' ]] || [[ "$CH" = 'Y' ]]; then
+      sudo -u ${MYUSER} rm ~/.ssh/id_rsa*
       DO_SSH=1
     fi
   else
     DO_SSH=1
   fi
-  if [ "$DO_SSH" -eq 1 ]; then
-    sudo -u $MYUSER ssh-keygen -f /home/$MYUSER/.ssh/id_rsa -P ''
-    eval $(sudo -u $MYUSER ssh-agent)
-    ssh-add /home/$MYUSER/.ssh/id_rsa
+  if [[ "$DO_SSH" -eq 1 ]]; then
+    sudo -u ${MYUSER} ssh-keygen -f /home/${MYUSER}/.ssh/id_rsa -P ''
+    eval $(sudo -u ${MYUSER} ssh-agent)
+    ssh-add /home/${MYUSER}/.ssh/id_rsa
   fi
 fi
 
-if [ "$ADD_ADDITIONAL_SSH_KEY_FOR_GIT" -eq 1 ]; then
+if [[ "$ADD_ADDITIONAL_SSH_KEY_FOR_GIT" -eq 1 ]]; then
   echo ---------- Adding additional SSH key for git
   DO_SSH=0
-  if [ -e /home/$MYUSER/.ssh/$ADDITIONAL_SSH_KEY_NAME ]; then
-    read -p "File ~/.ssh/$ADDITIONAL_SSH_KEY_NAME already exists! Do you want do delete it? [Y/n]: " CH
-    if [ "$CH" = '' ] || [ "$CH" = 'y' ] || [ "$CH" = 'Y' ]; then
-      sudo -u $MYUSER rm "/home/$MYUSER/.ssh/$ADDITIONAL_SSH_KEY_NAME*"
+  if [[ -e /home/${MYUSER}/.ssh/${ADDITIONAL_SSH_KEY_NAME} ]]; then
+    read -p "File ~/.ssh/${ADDITIONAL_SSH_KEY_NAME} already exists! Do you want do delete it? [Y/n]: " CH
+    if [[ "$CH" = '' ]] || [[ "$CH" = 'y' ]] || [[ "$CH" = 'Y' ]]; then
+      sudo -u ${MYUSER} rm "/home/${MYUSER}/.ssh/${ADDITIONAL_SSH_KEY_NAME}*"
       DO_SSH=1
     fi
   else
     DO_SSH=1
   fi
-  if [ "$DO_SSH" -eq 1 ]; then
-    sudo -u $MYUSER ssh-keygen -f /home/$MYUSER/.ssh/$ADDITIONAL_SSH_KEY_NAME -P ''
-    eval $(sudo -u $MYUSER ssh-agent)
-    ssh-add /home/$MYUSER/.ssh/$ADDITIONAL_SSH_KEY_NAME
+  if [[ "${DO_SSH}" -eq 1 ]]; then
+    sudo -u ${MYUSER} ssh-keygen -f /home/${MYUSER}/.ssh/${ADDITIONAL_SSH_KEY_NAME} -P ''
+    eval $(sudo -u ${MYUSER} ssh-agent)
+    ssh-add /home/${MYUSER}/.ssh/${ADDITIONAL_SSH_KEY_NAME}
   fi
 fi
 
-if [ "$CREATE_ALIASES" -eq 1 ]; then
+if [[ "$CREATE_ALIASES" -eq 1 ]]; then
   echo ---------- Creating aliases
-  su - $MYUSER -c "echo '${BASH_ALIASES}' > /home/$MYUSER/.bash_aliases"
+  su - ${MYUSER} -c "echo '${BASH_ALIASES}' > /home/${MYUSER}/.bash_aliases"
 fi
 
-if [ "$CREATE_SSH_CONFIG_FILE" -eq 1 ]; then
+if [[ "$CREATE_SSH_CONFIG_FILE" -eq 1 ]]; then
   echo ---------- Creating ssh .config example
-  su - $MYUSER -c "echo '${SSH_CONFIG}' > /home/$MYUSER/.ssh/config"
+  su - ${MYUSER} -c "echo '${SSH_CONFIG}' > /home/${MYUSER}/.ssh/config"
 fi
 
-if [ "$INSTALL_CHROME" -eq 1 ]; then
+if [[ "$INSTALL_CHROME" -eq 1 ]]; then
   echo ---------- Installing latest Chrome
   wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
   dpkg -i google-chrome-stable_current_amd64.deb
@@ -310,51 +292,51 @@ if [ "$INSTALL_CHROME" -eq 1 ]; then
   rm google-chrome-stable_current_amd64.deb
 fi
 
-if [ "$INSTALL_PYCHARM" -eq 1 ]; then
+if [[ "$INSTALL_PYCHARM" -eq 1 ]]; then
   echo ---------- Installing PyCharm
   snap install pycharm-community --classic
 fi
 
-if [ "$INSTALL_PYCHARM_PRO" -eq 1 ]; then
+if [[ "$INSTALL_PYCHARM_PRO" -eq 1 ]]; then
   echo ---------- Installing PyCharm Pro
   wget https://download-cf.jetbrains.com/python/pycharm-professional-2020.1.1.tar.gz
   tar xzf pycharm-professional-2020.1.1.tar.gz
   mv pycharm-2020.1.1 /opt/pycharm
-  chown -R $MYUSER:$MYUSER /opt/pycharm
+  chown -R ${MYUSER}:${MYUSER} /opt/pycharm
   rm pycharm-professional-2020.1.1.tar.gz
 fi
 
-if [ "$INSTALL_VSCODE" -eq 1 ]; then
+if [[ "$INSTALL_VSCODE" -eq 1 ]]; then
   echo ---------- Installing VSCode
   snap install code --classic
 fi
 
-if [ "$INSTALL_CHROMIUM" -eq 1 ]; then
+if [[ "$INSTALL_CHROMIUM" -eq 1 ]]; then
   echo ---------- Installing Chromium
   snap install chromium
 fi
 
-if [ "$INSTALL_SUBLIME" -eq 1 ]; then
+if [[ "$INSTALL_SUBLIME" -eq 1 ]]; then
   echo ---------- Installing Sublime
   snap install sublime-text --classic
 fi
 
-if [ "$INSTALL_KRITA" -eq 1 ]; then
+if [[ "$INSTALL_KRITA" -eq 1 ]]; then
   echo ---------- Installing Krita
   snap install krita
 fi
 
-if [ "$INSTALL_AUDACITY" -eq 1 ]; then
+if [[ "$INSTALL_AUDACITY" -eq 1 ]]; then
   echo ---------- Installing Audacity
   snap install audacity
 fi
 
-if [ "$INSTALL_POSTMAN" -eq 1 ]; then
+if [[ "$INSTALL_POSTMAN" -eq 1 ]]; then
     echo ---------- Installing Postman
     snap install postman
 fi
 
-if [ "$INSTALL_SHUTTER" -eq 1 ]; then
+if [[ "$INSTALL_SHUTTER" -eq 1 ]]; then
   echo ---------- Installing Shutter
   apt -y install shutter
   # snap install shutter
@@ -371,22 +353,22 @@ if [ "$INSTALL_SHUTTER" -eq 1 ]; then
   killall shutter
 fi
 
-if [ "$INSTALL_FORTICLIENT_VPN" -eq 1 ]; then
+if [[ "$INSTALL_FORTICLIENT_VPN" -eq 1 ]]; then
   echo ---------- Installing Forticlient VPN
   wget https://hadler.me/files/forticlient-sslvpn_4.4.2333-1_amd64.deb
   dpkg -i forticlient-sslvpn_4.4.2333-1_amd64.deb
   rm forticlient-sslvpn_4.4.2333-1_amd64.deb
 fi
 
-if [ "$INSTALL_DOCKER" -eq 1 ]; then
+if [[ "$INSTALL_DOCKER" -eq 1 ]]; then
   echo ---------- Installing Docker
   curl -fsSL https://get.docker.com -o get-docker.sh
   sh get-docker.sh
   rm get-docker.sh
-  usermod -aG docker $MYUSER
+  usermod -aG docker ${MYUSER}
 fi
 
-if [ "$INSTALL_DROPBOX" -eq 1 ]; then
+if [[ "$INSTALL_DROPBOX" -eq 1 ]]; then
   echo ---------- Installing Dropbox
   wget https://linux.dropbox.com/packages/ubuntu/dropbox_2019.02.14_amd64.deb
   dpkg -i dropbox_2019.02.14_amd64.deb
@@ -394,20 +376,20 @@ if [ "$INSTALL_DROPBOX" -eq 1 ]; then
   rm dropbox_2019.02.14_amd64.deb
 fi
 
-if [ "$INSTALL_YOUTUBE_DL" -eq 1 ]; then
+if [[ "$INSTALL_YOUTUBE_DL" -eq 1 ]]; then
   echo ---------- Installing youtube-dl
   apt -y install ffmpeg
-  sudo -u $MYUSER pip3 install youtube-dl
+  sudo -u ${MYUSER} pip3 install youtube-dl
 fi
 
-if [ "$INSTALL_ACESTREAMPLAYER" -eq 1 ]; then
+if [[ "$INSTALL_ACESTREAMPLAYER" -eq 1 ]]; then
   echo ---------- Installing acestreamplayer
   snap install acestreamplayer
 fi
 
-if [ "$INSTALL_SAMBA" -eq 1 ]; then
+if [[ "$INSTALL_SAMBA" -eq 1 ]]; then
   echo ---------- Installing Samba
-  sudo -u $MYUSER mkdir -p "${SAMBA_SHARE_DIR}"
+  sudo -u ${MYUSER} mkdir -p "${SAMBA_SHARE_DIR}"
   apt -y install samba
   echo --- creating backup for samba config
   cp /etc/samba/smb.conf /etc/samba/smb-bk.conf
@@ -425,41 +407,41 @@ if [ "$INSTALL_SAMBA" -eq 1 ]; then
   ) | smbpasswd -s -a ${MYUSER}
 fi
 
-if [ "$INSTALL_FREECAD" -eq 1 ]; then
+if [[ "$INSTALL_FREECAD" -eq 1 ]]; then
   echo ---------- Installing FreeCAD
-  sudo -u $MYUSER mkdir -p /home/${MYUSER}/Soft
+  sudo -u ${MYUSER} mkdir -p /home/${MYUSER}/Soft
   cd /home/${MYUSER}/Soft
-  sudo -u $MYUSER wget https://github.com/FreeCAD/FreeCAD/releases/download/0.18.2/FreeCAD_0.18-16117-Linux-Conda_Py3Qt5_glibc2.12-x86_64.AppImage -P /home/${MYUSER}/Soft
+  sudo -u ${MYUSER} wget https://github.com/FreeCAD/FreeCAD/releases/download/0.18.2/FreeCAD_0.18-16117-Linux-Conda_Py3Qt5_glibc2.12-x86_64.AppImage -P /home/${MYUSER}/Soft
   chmod +x FreeCAD_0.18-16117-Linux-Conda_Py3Qt5_glibc2.12-x86_64.AppImage
 fi
 
-if [ "$INSTALL_HYDROGEN" -eq 1 ]; then
+if [[ "$INSTALL_HYDROGEN" -eq 1 ]]; then
   echo ---------- Installing Hydrogen
   apt -y install hydrogen
 fi
 
-if [ "$INSTALL_CALIBRE" -eq 1 ]; then
+if [[ "$INSTALL_CALIBRE" -eq 1 ]]; then
   echo "---------- Installing Calibre (ebook manager)"
   wget -nv -O- https://download.calibre-ebook.com/linux-installer.sh | sh /dev/stdin
 fi
 
-if [ "$INSTALL_BLENDER" -eq 1 ]; then
+if [[ "$INSTALL_BLENDER" -eq 1 ]]; then
   echo "---------- Installing Blender (3D / video editor)"
   apt -y install blender
 fi
 
-if [ "$INSTALL_OPENSHOT" -eq 1 ]; then
+if [[ "$INSTALL_OPENSHOT" -eq 1 ]]; then
   echo "---------- Installing Openshot (video editor)"
   apt -y install openshot-qt
 fi
 
-if [ "$FIX_CALCULATOR_KEYBOARD_SHORTCUT" -eq 1 ]; then
+if [[ "$FIX_CALCULATOR_KEYBOARD_SHORTCUT" -eq 1 ]]; then
   echo ---------- Fix calculator shortcut
   snap remove gnome-calculator
   apt -y install gnome-calculator
 fi
 
-if [ "$INSTALL_MYSQL_WORKBENCH" -eq 1 ]; then
+if [[ "$INSTALL_MYSQL_WORKBENCH" -eq 1 ]]; then
   echo ---------- Installing MySQL Workbench
   wget https://cdn.mysql.com/Downloads/MySQLGUITools/mysql-workbench-community_8.0.20-1ubuntu20.04_amd64.deb
   dpkg -i mysql-workbench-community_8.0.20-1ubuntu20.04_amd64.deb
@@ -467,7 +449,7 @@ if [ "$INSTALL_MYSQL_WORKBENCH" -eq 1 ]; then
   rm mysql-workbench-community_8.0.20-1ubuntu20.04_amd64.deb
 fi
 
-if [ "$INSTALL_MONGODB_COMPASS" -eq 1 ]; then
+if [[ "$INSTALL_MONGODB_COMPASS" -eq 1 ]]; then
   echo ---------- Installing MySQL Compass
   wget https://downloads.mongodb.com/compass/mongodb-compass_1.20.4_amd64.deb
   dpkg -i mongodb-compass_1.20.4_amd64.deb
@@ -479,62 +461,63 @@ if [[ ${ADD_NEW_TEXT_FILE_TEMPLATE} -eq 1 ]]; then
   sudo -u ${MYUSER} touch /home/${MYUSER}/Templates/New\ File.txt
 fi
 
-if [ "$INSTALL_MYSQL_DOCKER" -eq 1 ]; then
+if [[ "$INSTALL_MYSQL_DOCKER" -eq 1 ]]; then
   echo ---------- Installing MySQL Docker
   docker run --name mysql-container -e MYSQL_ROOT_PASSWORD="$MYSQL_ROOT_PASSWORD" -e MYSQL_ROOT_HOST=172.17.0.1 \
-    -p 3306:3306 -v /home/$MYUSER/mysql:/var/lib/mysql -d mysql/mysql-server:5.7 \
-    --character-set-server=utf8 --collation-server=utf8_general_ci
-  sudo -u $MYUSER mkdir -p /home/$MYUSER/.config/autostart
-  su - $MYUSER -c "echo '[Desktop Entry]
-Name=MySQL
-Exec=docker start mysql-container
-Type=Application
-X-GNOME-Autostart-enabled=true
-' > /home/$MYUSER/.config/autostart/mysql-docker.desktop"
+    -p 3306:3306 -v /home/${MYUSER}/mysql:/var/lib/mysql -d mysql/mysql-server:5.7 \
+    --character-set-server=utf8 --collation-server=utf8_general_ci --restart=unless-stopped
+#  sudo -u ${MYUSER} mkdir -p /home/${MYUSER}/.config/autostart
+#  su - ${MYUSER} -c "echo '[Desktop Entry]
+#Name=MySQL
+#Exec=docker start mysql-container
+#Type=Application
+#X-GNOME-Autostart-enabled=true
+#' > /home/${MYUSER}/.config/autostart/mysql-docker.desktop"
 fi
 
-if [ "$INSTALL_ACTIVEMQ_DOCKER" -eq 1 ]; then
+if [[ "$INSTALL_ACTIVEMQ_DOCKER" -eq 1 ]]; then
   echo ---------- Installing ActiveMQ
-  docker run --name activemq-container -p 61613:61613 -p 8161:8161 -d rmohr/activemq
-  sudo -u $MYUSER mkdir -p /home/$MYUSER/.config/autostart
-  su - $MYUSER -c "echo '[Desktop Entry]
-Name=ActiveMQ
-Exec=docker start activemq-container
-Type=Application
-X-GNOME-Autostart-enabled=true
-' > /home/$MYUSER/.config/autostart/activemq.desktop"
+  docker run --name activemq-container -p 61613:61613 -p 8161:8161 -d rmohr/activemq --restart=unless-stopped
+#  sudo -u ${MYUSER} mkdir -p /home/${MYUSER}/.config/autostart
+#  su - ${MYUSER} -c "echo '[Desktop Entry]
+#Name=ActiveMQ
+#Exec=docker start activemq-container
+#Type=Application
+#X-GNOME-Autostart-enabled=true
+#' > /home/${MYUSER}/.config/autostart/activemq.desktop"
 fi
 
-if [ "$INSTALL_MONGODB_DOCKER" -eq 1 ]; then
+if [[ "$INSTALL_MONGODB_DOCKER" -eq 1 ]]; then
   # https://hub.docker.com/_/mongo
   echo ---------- Installing MongoDB
   docker run --name mongodb-container \
      -e MONGO_INITDB_ROOT_USERNAME=$MONGODB_USER \
      -e MONGO_INITDB_ROOT_PASSWORD=$MONGODB_PWD \
-     -v /home/$MYUSER/mongodb:/data/db \
+     -v /home/${MYUSER}/mongodb:/data/db \
      -p 27017:27017 \
-     -d mongo:3.6-xenial
-  sudo -u $MYUSER mkdir -p /home/$MYUSER/.config/autostart
-  su - $MYUSER -c "echo '[Desktop Entry]
-Name=MongoDB
-Exec=docker start mongodb-container
-Type=Application
-X-GNOME-Autostart-enabled=true
-' > /home/$MYUSER/.config/autostart/mongodb-docker.desktop"
+     -d mongo:3.6-xenial \
+     --restart=unless-stopped
+#  sudo -u ${MYUSER} mkdir -p /home/${MYUSER}/.config/autostart
+#  su - ${MYUSER} -c "echo '[Desktop Entry]
+#Name=MongoDB
+#Exec=docker start mongodb-container
+#Type=Application
+#X-GNOME-Autostart-enabled=true
+#' > /home/${MYUSER}/.config/autostart/mongodb-docker.desktop"
 fi
 
 # favorites bar
-if [ "$SET_DOCK_POSITION_BOTTOM" -eq 1 ]; then
+if [[ "$SET_DOCK_POSITION_BOTTOM" -eq 1 ]]; then
   echo ---------- Moving dock to bottom
-  su - $MYUSER -c "gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM"
+  su - ${MYUSER} -c "gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM"
 fi
 
-if [ "$SET_FAVORITES_BAR" -eq 1 ]; then
+if [[ "$SET_FAVORITES_BAR" -eq 1 ]]; then
   echo ---------- Setting favorites bar
-  su - $MYUSER -c "gsettings set org.gnome.shell favorite-apps \"['org.gnome.Terminal.desktop', 'google-chrome.desktop', 'sublime-text_subl.desktop', 'pycharm-community_pycharm-community.desktop', 'postman_postman.desktop', 'chromium_chromium.desktop', 'mysql-workbench.desktop', 'firefox.desktop', 'krita_krita.desktop', 'org.gnome.Nautilus.desktop']\""
+  su - ${MYUSER} -c "gsettings set org.gnome.shell favorite-apps \"['org.gnome.Terminal.desktop', 'google-chrome.desktop', 'sublime-text_subl.desktop', 'pycharm-community_pycharm-community.desktop', 'postman_postman.desktop', 'chromium_chromium.desktop', 'mysql-workbench.desktop', 'firefox.desktop', 'krita_krita.desktop', 'org.gnome.Nautilus.desktop']\""
 fi
 
-if [ "$INSTALL_YANDEXDISK" -eq 1 ]; then
+if [[ "$INSTALL_YANDEXDISK" -eq 1 ]]; then
   echo ---------- Installing Yandex Disk
   # original command with sudo [https://yandex.com/support/disk/cli-clients.html]
   # echo "deb http://repo.yandex.ru/yandex-disk/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/yandex-disk.list > /dev/null && wget http://repo.yandex.ru/yandex-disk/YANDEX-DISK-KEY.GPG -O- | sudo apt-key add - && sudo apt-get update && sudo apt install -y yandex-disk
@@ -544,11 +527,11 @@ fi
 
 # final prompts and notices
 echo done running Ubuntu checklist!
-if [ "$INSTALL_DROPBOX" -eq 1 ]; then
+if [[ "$INSTALL_DROPBOX" -eq 1 ]]; then
   echo '- Please complete the Dropbox installation and login'
 fi
 
-if [ "$ADD_SSH_KEY_FOR_GIT" -eq 1 ]; then
+if [[ "$ADD_SSH_KEY_FOR_GIT" -eq 1 ]]; then
   echo '- Please set SSH public key in Bitbucket and Github!'
 fi
 
