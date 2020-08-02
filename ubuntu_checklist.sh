1e89a2ffa00866d6eaace587b175cb08f59b5a3d
Oleg <oleg@work>
2020-08-02 14:13:09 +0300

    Docker Compose

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -396,6 +396,7 @@ fi
 if [[ "$INSTALL_DOCKER_COMPOSE" -eq 1 ]]; then
   echo ---------- Installing Docker Compose
   curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  chmod +x /usr/local/bin/docker-compose
 fi
 
 if [[ "$INSTALL_DROPBOX" -eq 1 ]]; then
