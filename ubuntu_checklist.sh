c84a1d92aa6b9a895e40913c611f3816d1f299bc
Oleg <oleg@work>
2021-06-21 18:36:15 +0300

    $INSTALL_SUBLIME

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -425,6 +425,7 @@ if [[ "$INSTALL_SUBLIME" -eq 1 ]]; then
 #  snap install sublime-text --classic
   # https://www.howtoforge.com/installation-of-sublime-text-on-ubuntu-20-04/
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+  echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
   apt-get update
   apt-get install sublime-text
 fi
