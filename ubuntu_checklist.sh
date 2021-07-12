4382dc135791512ea448b539f972fde5e7f6abf0
Oleg <oleg@work>
2021-07-12 14:51:31 +0300

    wip

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -570,10 +570,11 @@ fi
 
 if [[ "$INSTALL_MYSQL_WORKBENCH" -eq 1 ]]; then
   echo ---------- Installing MySQL Workbench
-  wget https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community_8.0.22-1ubuntu20.04_amd64.deb
-  dpkg -i mysql-workbench-community_8.0.22-1ubuntu20.04_amd64.deb
-  apt -y install -f
-  rm mysql-workbench-community_8.0.22-1ubuntu20.04_amd64.deb
+#  wget https://cdn.mysql.com//Downloads/MySQLGUITools/mysql-workbench-community_8.0.22-1ubuntu20.04_amd64.deb
+#  dpkg -i mysql-workbench-community_8.0.22-1ubuntu20.04_amd64.deb
+#  apt -y install -f
+#  rm mysql-workbench-community_8.0.22-1ubuntu20.04_amd64.deb
+  mysql-workbench-community
 fi
 
 if [[ "$INSTALL_MONGODB_COMPASS" -eq 1 ]]; then
