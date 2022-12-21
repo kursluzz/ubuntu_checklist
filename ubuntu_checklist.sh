97b92044c366489652627d68d878bfcc2613c1bb
Oleg <oleg@bonditglobal.com>
2022-12-21 10:43:01 +0200

    sublime from repo

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -474,8 +474,13 @@ fi
 
 if [[ "$INSTALL_SUBLIME" -eq 1 ]]; then
   echo ---------- Installing Sublime
-  wget https://download.sublimetext.com/sublime-text_build-4126_amd64.deb
-  sudo dpkg -i sublime-text_build-4126_amd64.deb
+  # https://www.sublimetext.com/docs/linux_repositories.html
+#  wget https://download.sublimetext.com/sublime-text_build-4126_amd64.deb
+#  sudo dpkg -i sublime-text_build-4126_amd64.deb
+  wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
+  echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+  sudo apt update
+  sudo apt install -y sublime-text
 fi
 
 if [[ "$INSTALL_KRITA" -eq 1 ]]; then
