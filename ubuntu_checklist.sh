ceb1ef4277721956866825dc2c488381671604ec
Oleg <oleg@bonditglobal.com>
2023-10-05 16:11:38 +0300

    w

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -300,10 +300,9 @@ if [[ "$INSTALL_MYSQL_PYTHON_DEPENDENCIES" -eq 1 ]]; then
   if [[ "$OS" = "Debian" ]]; then
     sudo apt -y install default-libmysqlclient-dev libssl-dev
   else
+    sudo apt install -y default-mysql-client
     sudo apt install -y libmysqlclient-dev
-    wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
-    sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb
-    rm libssl1.1_1.1.1f-1ubuntu2.16_amd64.deb  
+    sudo apt install -y pkg-config
   fi
 fi
 
