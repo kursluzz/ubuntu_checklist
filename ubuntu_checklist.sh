5c3a02eb003d46055bebe129cccc4577abfc9d30
Oleg <oleg@bonditglobal.com>
2025-06-16 15:23:35 +0300

    w

--- ubuntu_checklist.sh
+++ ubuntu_checklist.sh
@@ -485,11 +485,12 @@ if [[ "$INSTALL_VSCODE" -eq 1 ]]; then
   sudo apt-get install wget gpg
   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
   sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
   rm -f packages.microsoft.gpg
   sudo apt install apt-transport-https
   sudo apt update
-  sudo apt install -y code
+  sudo apt install code # or code-insiders
+
 fi
 
 if [[ "$INSTALL_TEAMS" -eq 1 ]]; then
